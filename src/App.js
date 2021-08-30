0104bc611b021142fba61e49ef84ff93ad2152db
unknown <pushkartungare@gmail.com>
2021-08-30 20:21:52 +0530

    Update parameter

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 class App extends Component {
   componentDidMount() {
     const api = 'https://0vlxd0lfec.execute-api.ap-south-1.amazonaws.com/dev';
-    const data = { 'name' : 'Devops' };
+    const data = { 'name' : 'Devops Engineering' };
     axios
       .post(api, data)
       .then((response) => {
